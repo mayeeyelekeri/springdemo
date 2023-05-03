5fa93974ef4265cdfb193783e04b0d8a0aeaf59d
Mahesh <yelekeri@gmail.com>
2023-05-03 13:39:15 +0000

    removed container and make it success by default

--- codedeploy/scripts/start_server.sh
+++ codedeploy/scripts/start_server.sh
@@ -4,5 +4,5 @@ id
 ls -lrt /tmp 
 ls -lrt ~
 echo $HOME
-#cd /tmp; docker build -t springdemo:latest .
-#docker run -p 8080:8080 -d springdemo:latest 
+cd /tmp; docker build -t springdemo:latest .
+docker run -p 8080:8080 -d springdemo:latest 
